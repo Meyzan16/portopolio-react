a97aab41346e8a2fb8a59fdd0afb54b0d091a873
Meyzan16 <Meyzan1605@gmail.com>
2023-03-29 14:38:22 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import Aos from 'aos';
+import './App.css';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  useEffect(()=> {
+    Aos.init();
+  }, []);
+
+  return <>
+    <Header />
+        <main>
+          
+        </main>
+
+    <Footer />
+  </>
+
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#8873ef",
+        headingcolor: "#081e21",
+        smallTextColor: "#193256",
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      // screens:{
+      //   '2xl':'1320px',
+      // },
+    },
+  },
+  plugins: [],
+}
